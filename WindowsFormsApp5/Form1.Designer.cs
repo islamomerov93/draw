0cc7595004a9eb6e024816f5bf638f8d62e6062c
omerov islam <islamomerov@outlook.com>
2019-01-10 21:46:21 +0400

    Add project files.

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // showLbl
+            // 
+            this.showLbl.AutoSize = true;
+            this.showLbl.Location = new System.Drawing.Point(331, 169);
+            this.showLbl.Name = "showLbl";
+            this.showLbl.Size = new System.Drawing.Size(32, 13);
+            this.showLbl.TabIndex = 0;
+            this.showLbl.Text = "show";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label showLbl;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        Point StartPoint = new Point();
+        Point EndPoint = new Point();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            StartPoint.X = e.X;
+            StartPoint.Y = e.Y;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                EndPoint.X = e.X;
+                EndPoint.Y = e.Y;
+                Label label = new Label();
+
+                if (EndPoint.X < StartPoint.X) label.Location = EndPoint;
+                else label.Location = StartPoint;
+                label.Size = new Size(Math.Abs(EndPoint.X - StartPoint.X), Math.Abs(EndPoint.Y - StartPoint.Y));
+                label.BackColor = Color.Black;
+                Controls.Add(label);
+            }
+        }
+    }
+}
